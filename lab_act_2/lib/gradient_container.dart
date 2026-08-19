256474e22b0dd89ef121da6f75fad687b1b5550b
LAB12_USER ON WS07 <L12X19W07@LabHCI.net>
2026-08-19 20:21:15 +0800

    Create gradient_container.dart class

---
+++ lab_act_2/lib/gradient_container.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer({super.key});
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+          colors: [Colors.orange, Colors.yellow],
+        ),
+      ),
+      child: Center(child: Text('Hello World')),
+    );
+  }
+}
