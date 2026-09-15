e645927689b63c92724a5f07d9560908ec701a7d
Hamilcar J Edera Jr <hamilcar.jadraque@gmail.com>
2026-09-15 21:25:33 +0800

    Quiz Screen

--- lab_act_2/lib/main.dart
+++ lab_act_2/lib/main.dart
@@ -1,6 +1,79 @@
 import 'package:flutter/material.dart';
-import 'gradient_container.dart';
+
 
 void main() {
-  runApp( MaterialApp(home: Scaffold(body: GradientContainer())));
+  runApp(const QuizApp());
+
+}
+
+class QuizApp extends StatelessWidget {
+   const QuizApp({super.key});
+  
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: const QuizPage(),
+    );
+  }
+}
+
+class QuizPage extends StatefulWidget {
+  const QuizPage({super.key});
+
+  @override
+  State<QuizPage> createState() => _QuizPageState();
+}
+
+class _QuizPageState extends State<QuizPage> {
+  String question = "What is the capital of Philippines?";
+
+  List<String> options = [
+    "Cebu",
+    "Davao",
+    "Manila",
+    "Baguio"
+  ];
+    @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Quiz App"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              question,
+              style: const TextStyle(
+                fontSize: 22,
+              ),
+              textAlign: TextAlign.center,
+            ),
+
+            const SizedBox(height: 30),
+
+            for (String choice in options)
+              Container(
+                width: double.infinity,
+                margin: const EdgeInsets.only(bottom: 10),
+                child: ElevatedButton(
+                  onPressed: () {},
+                  child: Text(choice),
+                ),
+              ),
+
+            const SizedBox(height: 20),
+
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text("Next Question"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 }
